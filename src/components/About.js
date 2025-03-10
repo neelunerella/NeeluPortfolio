7fe22bb6b278e18ae928635f4cb8e694c6129a6e
neelunerella <n.neelotpala@gmail.com>
2025-03-09 23:19:59 -0500

    Added Project Section

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,12 @@
+import React from "react";
+
 export default function About() {
     return (
         <section id="about">
             <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md-mb-0 items-center text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black">
-                        Hi, I'm Neelu
+                        Hi, I'm Neelu! 
                         <br className="hidden lg:inline-block" /> I am passionate about developing the future of AI.
                     </h1>
                     <p className="mb-8 leading-relaxed">
@@ -30,7 +32,7 @@ export default function About() {
                     <img
                     className="object-cover object-center rounded"
                     alt="hero"
-                    src="./coding.svg"
+                    src="'./Portfolio Picture.JPG'"
                     />
                 </div>
             </div>
