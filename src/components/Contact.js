2802eb80307784b4092805e94b4272fd05ee2871
neelunerella <n.neelotpala@gmail.com>
2025-03-10 01:57:31 -0500

    added navbar and contact info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,1 +1,78 @@
-export default function Contact() {}
+import React from "react";
+
+export default function Contact() {
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+
+    function encode(data) {
+        return Object.keys(data).map(
+            (key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key])
+        ).join("&");
+    }
+
+    function handelSubmit(e) {
+        e.preventDefault();
+        fetch("/", {
+            method: "POST",
+            headers: {"Content-Type": "application/x-www-form-urlencoded"},
+            body: encode({"form-name": "contact", name, email, message}),
+        })
+        .then(() => alert("Message sent!"))
+        .catch((error) => alert(error));
+    }
+    
+    return (
+        <section id="contact" className="relative">
+            <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+                <form
+                netlify
+                name="contact"
+                className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
+                    <h2 className="text-white sm:text-4xl text-3xl m-1 font-medium title-font">
+                        Contact Me
+                    </h2>
+                    <div className="relative mb-4">
+                        <label htmlFor="name" className="leading-7 text-sm text-gray-400">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            name="name"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label htmlFor="email" className="leading-7 text-sm text-gray-400">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outlline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <div className="relative mb-4">
+                        <label
+                        htmlFor="message"
+                        className="leading-7 text-sm text-gray-400">
+                            Mesage
+                        </label>
+                        <textarea
+                            id="message"
+                            name="message"
+                            className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-l">
+                            Submit
+                        </button>
+                </form>
+            </div>
+        </section>
+    );
+}
